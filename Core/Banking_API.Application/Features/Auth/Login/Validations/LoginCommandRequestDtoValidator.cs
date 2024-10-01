2358a880dc86a27173c574cb4da8d0dca0ed2c64
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-10-01 13:33:33 +0300

    Fluent Validation was added(Login Validations).

--- Core/Banking_API.Application/Features/Auth/Login/Validations/LoginCommandRequestDtoValidator.cs
+++ Core/Banking_API.Application/Features/Auth/Login/Validations/LoginCommandRequestDtoValidator.cs
@@ -1,5 +1,4 @@
 ﻿using Banking_API.Application.Features.Auth.Login.Commands;
-using Banking_API.Application.Features.Auth.Login.Dtos;
 using FluentValidation;
 
 namespace Banking_API.Application.Features.Auth.Login.Validations
@@ -8,8 +7,15 @@ namespace Banking_API.Application.Features.Auth.Login.Validations
     {
         public LoginCommandRequestDtoValidator()
         {
-      
-         
+
+            RuleFor(x => x.LoginRequestDto.Email)
+               .NotEmpty().WithMessage("Email field cannot be empty.")
+               .EmailAddress().WithMessage("Please enter a valid email address.");
+
+            RuleFor(x => x.LoginRequestDto.Password)
+               .NotEmpty().WithMessage("Password field cannot be empty.")
+               .MinimumLength(6).WithMessage("Password must be at least 6 characters long.")
+               .MaximumLength(20).WithMessage("Password cannot exceed 20 characters.");
         }
     }
 }
